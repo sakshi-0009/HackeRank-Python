8aaf28bbe0efb93017728385cb46906e0e3d90a6
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2022-10-14 10:05:39 +0530

    Create Find a string.py

---
+++ Find a string.py
@@ -0,0 +1,23 @@
+# Question : Output the integer number indicating the total number of occurrences of the substring in the original string.
+
+# Answer :
+# Python Program Code :
+
+def count_substring(string, sub_string):
+    count=0
+    for i in range(0, len(string)-len(sub_string)+1):
+        if string[i] == sub_string[0]:
+            flag=1
+            for j in range (0, len(sub_string)):
+                if string[i+j] != sub_string[j]:
+                    flag=0
+                    break
+            if flag==1:
+                count += 1
+    return count
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
