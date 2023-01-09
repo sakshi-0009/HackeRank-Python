0b3da43be37c52dc145507c1f974f0531497767b
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-01-09 09:31:22 +0530

    Create Collections.OrderedDict().py

---
+++ Collections.OrderedDict().py
@@ -0,0 +1,17 @@
+'''Task :-
+You are the manager of a supermarket.
+You have a list of N items together with their prices that consumers bought on a particular day.
+Your task is to print each item_name and net_price in order of its first occurrence.
+
+item_name = Name of the item.
+net_price = Quantity of the item sold multiplied by the price of each item.'''
+
+# Answer :-
+
+from collections import OrderedDict
+d = OrderedDict()
+for _ in range(int(input())):
+    item, space, quantity = input().rpartition(' ')
+    d[item] = d.get(item, 0) + int(quantity)
+for item, quantity in d.items():
+    print(item, quantity)
